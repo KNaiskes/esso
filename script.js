1d5e9efed7a00ec8054f2d2220d41d64f7987814
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-02-22 21:01:28 +0200

    removed hours and minutes

--- script.js
+++ script.js
@@ -6,8 +6,5 @@ var update = setInterval(function() {
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m ";
+    document.getElementById("demo").innerHTML = days + " days left ";
 }, 1000);
