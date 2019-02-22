e4f425ef970c76f34bb2d7ff8c0ce9849f84ab20
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-02-22 21:44:27 +0200

    added message for the final day

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ var update = setInterval(function() {
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
     document.getElementById("demo").innerHTML = days + " days left ";
+
+    if(distance < 0) {
+        clearInterval(esso);
+        document.getElementById("demo").innerHTML = "The day you have been waiting has come!";
+    }
 }, 1000);
